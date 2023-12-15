d8f3917fdc8854e93e4df363e256b1e32f7344f9
Danny <danny@lilytronics.nl>
2023-12-15 16:48:38 +0100

    add various test class for testing the test suite base class

---
+++ unit_test/test_classes/test_class_pass.py
@@ -0,0 +1,18 @@
+"""
+Test class for use with the unit test.
+"""
+
+from unit_test.test_suite import TestSuite
+
+
+class TestClassPass(TestSuite):
+
+    def test_01_pass_by_return_none(self):
+        return None
+
+    def test_02_pass_by_return_true(self):
+        return True
+
+
+if __name__ == '__main__':
+    TestClassPass().run()
