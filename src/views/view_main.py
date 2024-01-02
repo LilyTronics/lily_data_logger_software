753c11e285ca5544b9b9521579034ca138cb1913
Danny <danny@lilytronics.nl>
2024-01-02 18:35:37 +0100

    add measurement data table

--- src/views/view_main.py
+++ src/views/view_main.py
@@ -3,7 +3,7 @@ Main view for the application
 """
 
 import lily_unit_test
-import wx
+import wx.grid
 
 from src.models.image_data import ImageData
 from wx.dataview import DataViewTreeCtrl
@@ -16,6 +16,7 @@ class ViewMain(wx.Frame):
     _LIST_COL_INDEX_SIZE = 30
     _LIST_COL_STEP_SIZE = 120
     _LIST_COL_DATA_SIZE = 120
+    _TABLE_MIN_COL_WIDTH = 100
 
     _MINIMUM_WINDOW_SIZE = (1100, 700)
 
@@ -26,6 +27,7 @@ class ViewMain(wx.Frame):
         lab_box = wx.BoxSizer(wx.HORIZONTAL)
         lab_box.Add(self._create_instruments_controls(panel), 20, wx.EXPAND | wx.TOP | wx.LEFT | wx.BOTTOM, self._GAP)
         lab_box.Add(self._create_process_box(panel), 30, wx.EXPAND | wx.ALL, self._GAP)
+        lab_box.Add(self._create_measurement_box(panel), 50, wx.EXPAND | wx.TOP | wx.RIGHT | wx.BOTTOM, self._GAP)
 
         panel.SetSizer(lab_box)
 
@@ -69,7 +71,7 @@ class ViewMain(wx.Frame):
         box = wx.StaticBoxSizer(wx.StaticBox(parent, wx.ID_ANY, " Process: "), wx.VERTICAL)
 
         self._lst_process = wx.ListCtrl(parent, wx.ID_ANY, style=wx.LC_REPORT | wx.LC_SINGLE_SEL | wx.LC_HRULES |
-                                                                 wx.LC_VRULES)
+                                        wx.LC_VRULES)
         self._lst_process.SetInitialSize((-1, 100))
         self._lst_process.InsertColumn(0, '#', width=self._LIST_COL_INDEX_SIZE)
         self._lst_process.InsertColumn(1, 'Step', width=self._LIST_COL_STEP_SIZE)
@@ -88,6 +90,40 @@ class ViewMain(wx.Frame):
         box.Add(grid, 0, wx.ALL, self._GAP)
 
         return box
+
+    def _create_measurement_box(self, parent):
+        box = wx.StaticBoxSizer(wx.StaticBox(parent, wx.ID_ANY, ' Measurements: '), wx.VERTICAL)
+
+        grid_panel = wx.Panel(parent, wx.ID_ANY)
+        grid_panel.SetBackgroundColour('#abadb3')
+
+        self._grid_measurements = wx.grid.Grid(grid_panel, wx.ID_ANY)
+        self._grid_measurements.CreateGrid(0, 1)
+        self._grid_measurements.SetColLabelValue(0, 'Time')
+        self._grid_measurements.SetColMinimalWidth(0, self._TABLE_MIN_COL_WIDTH)
+        self._grid_measurements.AutoSizeColLabelSize(0)
+        self._grid_measurements.EnableEditing(False)
+        self._grid_measurements.EnableDragRowSize(False)
+        self._grid_measurements.EnableDragColMove(False)
+        self._grid_measurements.EnableDragColSize(False)
+
+        btn_add_measurement = wx.Button(parent, wx.ID_ANY, 'Add')
+        btn_delete_measurement = wx.Button(parent, wx.ID_ANY, 'Delete')
+
+        grid = wx.GridBagSizer(self._GAP, self._GAP)
+        grid.Add(btn_add_measurement, (0, 0), wx.DefaultSpan)
+        grid.Add(btn_delete_measurement, (0, 1), wx.DefaultSpan)
+
+        grid_box = wx.BoxSizer(wx.VERTICAL)
+        grid_box.Add(self._grid_measurements, 1, wx.EXPAND | wx.ALL, 1)
+
+        grid_panel.SetSizer(grid_box)
+
+        box.Add(grid_panel, 1, wx.EXPAND | wx.ALL, self._GAP)
+        box.Add(grid, 0, wx.ALL, self._GAP)
+
+        return box
+
 
 class ViewMainTest(lily_unit_test.TestSuite):
 
