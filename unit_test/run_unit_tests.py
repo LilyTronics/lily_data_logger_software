d21828d9e1a41c57390a6554de68953b63748bd3
Danny <danny@lilytronics.nl>
2024-01-04 11:36:59 +0100

    set output folder and skip running the test suite base class

--- unit_test/run_unit_tests.py
+++ unit_test/run_unit_tests.py
@@ -2,12 +2,19 @@
 Runs all the unit tests
 """
 
+import os
+
 from lily_unit_test import TestRunner
 
 options = {
+    'report_folder': os.path.join(os.path.dirname(__file__), 'test_reports'),
     'create_html_report': True,
     'open_in_browser': True,
-    'no_log_files': True
+    'no_log_files': True,
+    # Exclude our own test suite
+    'exclude_test_suites': [
+        'TestSuite'
+    ]
 }
 
 TestRunner.run('../src', options)
