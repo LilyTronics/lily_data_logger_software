7a42b9ce26534e95a13d6f4b017b20be8085f51e
Danny <danny@lilytronics.nl>
2024-01-09 09:24:19 +0100

    add exe name and user folder

--- src/app_data.py
+++ src/app_data.py
@@ -2,7 +2,19 @@
 Application data
 """
 
+import os
+
 
 class AppData(object):
     APP_NAME = 'Lily Data Logger Studio'
     VERSION = '0.1'
+    EXE_NAME = 'LilyDataLoggerStudio'
+    USER_FOLDER = os.path.join(os.path.join(os.path.expanduser('~')), EXE_NAME)
+
+
+if __name__ == '__main__':
+
+    print('App name   :', AppData.APP_NAME)
+    print('App version:', AppData.VERSION)
+    print('Exe name   :', AppData.EXE_NAME)
+    print('User folder:', AppData.USER_FOLDER)
