e72070e00b90344c22fa8751786c5f98029fb800
Danny <danny@lilytronics.nl>
2024-01-14 22:02:37 +0100

    add unit test for interfaces

--- src/models/interfaces/__init__.py
+++ src/models/interfaces/__init__.py
@@ -2,6 +2,8 @@
 Interfaces package.
 """
 
+import lily_unit_test
+
 from src.models.interfaces.serial_port_interface import SerialPortInterface
 from src.models.interfaces.tcp_client_interface import TcpClientInterface
 from src.models.interfaces.udp_client_interface import UdpClientInterface
@@ -20,11 +22,20 @@ def get_interface_names():
 
 def get_interface_by_name(interface_name):
     matches = list(filter(lambda x: x.NAME == interface_name, _INTERFACES))
-    assert len(matches) == 1, 'Interface with name {} not found'.format(interface_name)
-    return matches[0]
+    if len(matches) == 1:
+        return matches[0]
+    return None
+
+
+class TestInterfaces(lily_unit_test.TestSuite):
+
+    def test_interfaces(self):
+        for name in get_interface_names():
+            self.log.debug('{:12}: {}'.format(name, get_interface_by_name(name)))
+        interface = get_interface_by_name('Invalid name')
+        self.fail_if(interface is not None, 'Unknown interface name did not return None')
 
 
 if __name__ == '__main__':
 
-    for name in get_interface_names():
-        print('{:12}: {}'.format(name, get_interface_by_name(name)))
+    TestInterfaces().run()
