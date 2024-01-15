94adc193e6b9053509545affcdf40a00b69c628f
Danny <danny@lilytronics.nl>
2024-01-15 07:56:59 +0100

    add get interface type

--- src/models/instrument.py
+++ src/models/instrument.py
@@ -7,8 +7,15 @@ import lily_unit_test
 
 class Instrument(object):
 
+    KEY_CHANNELS = 'channels'
+    KEY_INTERFACE = 'interface'
+    KEY_NAME = 'name'
+    KEY_TYPE = 'type'
+
     def __init__(self, instrument_definition):
-        self._name = instrument_definition.get('name', 'unknown')
+        self._name = instrument_definition.get(self.KEY_NAME, 'unknown')
+        self._interface = instrument_definition.get(self.KEY_INTERFACE, {})
+        self._channels = instrument_definition.get(self.KEY_CHANNELS, {})
 
     ##########
     # Public #
@@ -17,6 +24,9 @@ class Instrument(object):
     def get_name(self):
         return self._name
 
+    def get_interface_type(self):
+        return self._interface.get(self.KEY_TYPE, None)
+
 
 class TestInstrument(lily_unit_test.TestSuite):
 
@@ -24,6 +34,8 @@ class TestInstrument(lily_unit_test.TestSuite):
         instrument = Instrument({})
         self.fail_if(instrument.get_name() != 'unknown',
                      'The default name is not correct {}'.format(instrument.get_name()))
+        self.fail_if(instrument.get_interface_type() is not None,
+                     'The default interface type is not correct {}'.format(instrument.get_interface_type()))
 
 
 if __name__ == '__main__':
