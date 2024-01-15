bf4afc696723f63ff932637a34f80468f1eb18c4
Danny <danny@lilytronics.nl>
2024-01-15 08:51:30 +0100

    add info field and unit test

--- src/models/instruments/simulator_multimeter.py
+++ src/models/instruments/simulator_multimeter.py
@@ -2,9 +2,26 @@
 Instrument model for the multimeter simulator
 """
 
+import lily_unit_test
+
 from src.models.instrument import Instrument
 
 
 simulator_multimeter = Instrument({
-    'name': 'Simulator multimeter'
+    'name': 'Simulator multimeter',
+    'info': 'Read random values for voltage (4.9...5.1V) and current (0.39...0.41)'
 })
+
+
+class TestSimulatorMultimeter(lily_unit_test.TestSuite):
+
+    def test_properties(self):
+        self.fail_if(simulator_multimeter.get_name() != 'Simulator multimeter',
+                     'The name is not correct {}'.format(simulator_multimeter.get_name()))
+        self.fail_if(simulator_multimeter.get_info() == simulator_multimeter.DEFAULT_INFO,
+                     'The info has the default value')
+
+
+if __name__ == '__main__':
+
+    TestSimulatorMultimeter().run()
