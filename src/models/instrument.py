c65a935d2fc61fdde8e1c1064a6c60943ed6b1ba
Danny <danny@lilytronics.nl>
2024-01-15 08:43:35 +0100

    add constants for the default name and info

--- src/models/instrument.py
+++ src/models/instrument.py
@@ -13,9 +13,12 @@ class Instrument(object):
     KEY_NAME = 'name'
     KEY_TYPE = 'type'
 
+    DEFAULT_NAME = 'unknown'
+    DEFAULT_INFO = 'no info'
+
     def __init__(self, instrument_definition):
-        self._name = instrument_definition.get(self.KEY_NAME, 'unknown')
-        self._info = instrument_definition.get(self.KEY_INFO, 'no info')
+        self._name = instrument_definition.get(self.KEY_NAME, self.DEFAULT_NAME)
+        self._info = instrument_definition.get(self.KEY_INFO, self.DEFAULT_INFO)
         self._interface = instrument_definition.get(self.KEY_INTERFACE, {})
         self._channels = instrument_definition.get(self.KEY_CHANNELS, {})
 
@@ -37,9 +40,9 @@ class TestInstrument(lily_unit_test.TestSuite):
 
     def test_default_values(self):
         instrument = Instrument({})
-        self.fail_if(instrument.get_name() != 'unknown',
+        self.fail_if(instrument.get_name() != instrument.DEFAULT_NAME,
                      'The default name is not correct {}'.format(instrument.get_name()))
-        self.fail_if(instrument.get_info() != 'no info',
+        self.fail_if(instrument.get_info() != instrument.DEFAULT_INFO,
                      'The default info is not correct {}'.format(instrument.get_info()))
         self.fail_if(instrument.get_interface_type() is not None,
                      'The default interface type is not correct {}'.format(instrument.get_interface_type()))
