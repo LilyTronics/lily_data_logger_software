bad3243d214a3296edeaf51f27e84e6df741bbdb
Danny <danny@lilytronics.nl>
2024-01-15 08:36:29 +0100

    small change in test code

--- src/models/interfaces/__init__.py
+++ src/models/interfaces/__init__.py
@@ -32,7 +32,7 @@ class TestInterfaces(lily_unit_test.TestSuite):
     def test_interfaces(self):
         for name in get_interface_names():
             self.log.debug('{:12}: {}'.format(name, get_interface_by_name(name)))
-        interface = get_interface_by_name('Invalid name')
+        interface = get_interface_by_name('Unknown interface name')
         self.fail_if(interface is not None, 'Unknown interface name did not return None')
 
 
