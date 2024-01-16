ef9f1c8349d7895ed017d4e156d4588698a5145d
Danny <danny@lilytronics.nl>
2024-01-16 14:39:49 +0100

    add check for serial port

--- unit_test/test_environment/check_serial_loopback.py
+++ unit_test/test_environment/check_serial_loopback.py
@@ -1,42 +1,18 @@
 """
-Script that runs before all tests to set up the environment
+Check if a loopback is available on a serial port.
 """
 
-import os
 import serial
-import shutil
 import time
 
-from src.models.list_serial_ports import get_available_serial_ports
 
-
-def setup_environment(report_path, exclude_tests):
-    _clear_reports(report_path)
-    if not _serial_port_loopback_is_available():
-        exclude_tests.append('TestSerialPortInterface')
-
-
-def _clear_reports(report_path):
-    print('Clear report path {}'.format(report_path))
-    for item in os.listdir(report_path):
-        full_path = os.path.join(report_path, item)
-        if os.path.isfile(full_path):
-            os.remove(full_path)
-        elif os.path.isdir(full_path):
-            shutil.rmtree(full_path)
-        else:
-            raise Exception('ERROR: could not remove item: {}'.format(item))
-
-
-def _serial_port_loopback_is_available():
+def is_serial_loopback_available(serial_ports):
     loopback_found = False
-
     _CHECK_FOR_LOOPBACK_DATA = b'check_for_loopback'
     _TEST_COMMAND = b'serial_port_test'
     _RX_TIMEOUT = 1
-
     print('Checking all available ports for a loopback')
-    for port_name in get_available_serial_ports():
+    for port_name in serial_ports:
         print('Check for loopback on port: {}'.format(port_name))
         with serial.Serial(port_name, write_timeout=0.2) as s:
             try:
@@ -44,7 +20,6 @@ def _serial_port_loopback_is_available():
             except serial.serialutil.SerialTimeoutException:
                 print('Could not write data to port')
                 continue
-
             i = 5
             rx_data = b''
             while i > 0:
@@ -57,12 +32,11 @@ def _serial_port_loopback_is_available():
                 i -= 1
             else:
                 print('Loopback data not found (timeout)')
-
     return loopback_found
 
 
 if __name__ == '__main__':
 
-    tests_to_exclude = []
-    setup_environment('test_reports', tests_to_exclude)
-    print(tests_to_exclude)
+    from src.models.list_serial_ports import get_available_serial_ports
+
+    print(is_serial_loopback_available(get_available_serial_ports()))
