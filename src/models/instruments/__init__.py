2f70868001d7c24263762a38e3576b2b265805db
Danny <danny@lilytronics.nl>
2024-01-17 12:05:45 +0100

    instrument list includes user instruments

--- src/models/instruments/__init__.py
+++ src/models/instruments/__init__.py
@@ -2,9 +2,15 @@
 Instruments package.
 """
 
+import glob
+import json
 import os
 import re
+import shutil
+import unit_test
 
+from src.app_data import AppData
+from src.models.instrument import Instrument
 from src.models.instruments.power_supply_tti_pl303qmd_p import tti_pl303qmd_p
 from src.models.instruments.simulator_multimeter import simulator_multimeter
 from src.models.instruments.simulator_temperature_chamber import simulator_temperature_chamber
@@ -36,13 +42,19 @@ def get_instrument_by_name(instrument_name):
 
 
 def _get_instruments():
-    instruments = _INSTRUMENTS
-    instruments.extend(_get_user_instruments())
-    return instruments
-
-
-def _get_user_instruments():
-    instruments = []
+    instruments = _INSTRUMENTS.copy()
+    for item in glob.glob(os.path.join(AppData.USER_FOLDER, '*.json')):
+        try:
+            instrument_data = json.load(open(item, 'r'))
+            if type(instrument_data) is not dict:
+                raise
+            for key in [Instrument.KEY_NAME, Instrument.KEY_INFO, Instrument.KEY_INTERFACE,
+                        Instrument.KEY_INITIALIZE, Instrument.KEY_CHANNELS]:
+                if key not in instrument_data.keys():
+                    raise
+            instruments.append(Instrument(instrument_data))
+        except (Exception, ):
+            pass
     return instruments
 
 
@@ -62,6 +74,10 @@ class TestInstruments(TestSuite):
                 if len(matches) == 1:
                     n_found += 1
         self._n_instruments = n_found
+        self.log.debug('Copy test instrument to user folder')
+        shutil.copy2(os.path.join(os.path.dirname(unit_test.__file__), 'test_files', 'test_instrument.json'),
+                     os.path.join(AppData.USER_FOLDER, 'test_instrument.json'))
+        self._n_instruments += 1
 
     def test_instruments(self):
         count = 0
@@ -73,6 +89,11 @@ class TestInstruments(TestSuite):
         self.fail_if(count != self._n_instruments, 'The number of instruments is not correct')
         instrument = get_instrument_by_name('Unknown instrument name')
         self.fail_if(instrument is not None, 'Unknown instrument name did not return None')
+
+    @staticmethod
+    def teardown():
+        if os.path.isfile(os.path.join(AppData.USER_FOLDER, 'test_instrument.json')):
+            os.remove(os.path.join(AppData.USER_FOLDER, 'test_instrument.json'))
 
 
 if __name__ == '__main__':
