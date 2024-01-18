358a44c86b579c2682c36fb0a64058c66af5e7ed
Danny <danny@lilytronics.nl>
2024-01-18 08:55:37 +0100

    add functions for testing GUI

---
+++ unit_test/gui_unit_test.py
@@ -0,0 +1,28 @@
+"""
+Class with functions for GUI testing
+"""
+
+import wx
+
+
+class GuiUnitTest(object):
+
+    @staticmethod
+    def is_window_available(window_id):
+        return wx.Window.FindWindowById(window_id) is not None
+
+    @staticmethod
+    def get_value_from_window(window_id):
+        value = wx.Window.FindWindowById(window_id)
+        if value is not None:
+            if hasattr(value, 'GetValue'):
+                value = value.GetValue()
+            elif hasattr(value, 'GetLabel'):
+                value = value.GetLabel()
+            else:
+                raise Exception('Could not get value from window with ID {} ({})'.format(window_id, value))
+        return value
+
+    @staticmethod
+    def click_button(button_id):
+        wx.PostEvent(wx.Window.FindWindowById(button_id), wx.CommandEvent(wx.wxEVT_COMMAND_BUTTON_CLICKED, button_id))
--- unit_test/test_suite.py
+++ unit_test/test_suite.py
@@ -3,16 +3,25 @@ Our own test suite class derived from the lily-unit-test test suite.
 """
 
 import lily_unit_test
+import threading
+import time
+
+from unit_test.gui_unit_test import GuiUnitTest
 
 
 class TestSuite(lily_unit_test.TestSuite):
-    pass
-
-
-if __name__ == '__main__':
-
-    class _TestClass(TestSuite):
-        pass
-
-    _TestClass().run()
     
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.gui = GuiUnitTest()
+
+    @staticmethod
+    def start_thread(target):
+        t = threading.Thread(target=target)
+        t.daemon = True
+        t.start()
+        return t
+
+    @staticmethod
+    def sleep(sleep_time):
+        time.sleep(sleep_time)
