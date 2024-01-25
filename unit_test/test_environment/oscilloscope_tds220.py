f7e986d051c49e8b5fb20543bfad0a70cf0d885b
Danny <danny@lilytronics.nl>
2024-01-25 08:10:11 +0100

    refactored using the function for checking the serial port

--- unit_test/test_environment/oscilloscope_tds220.py
+++ unit_test/test_environment/oscilloscope_tds220.py
@@ -3,38 +3,18 @@ Detect the oscilloscope TDS220.
 - 9600 baud, flow control none, EOL LF
 """
 
-import serial
-import time
+from unit_test.test_environment.check_serial_port import check_serial_port
 
 
-def get_oscilloscope_serial_port(serial_ports):
-    id_command = b"*IDN?\n"
-    id_data = b"TEKTRONIX,TDS 220"
-
-    print("Checking all available ports for the TDS220")
+def get_tds220_serial_port(serial_ports):
+    print("Checking available ports for the TDS220")
     for port_name in serial_ports:
         print("Check for TDS220 on port: {}".format(port_name))
-        n_tries = 3
-        while n_tries > 0:
-            try:
-                with serial.Serial(port_name, 9600, write_timeout=0.2) as s:
-                    s.write(id_command)
-                    i = 5
-                    rx_data = b""
-                    while i > 0:
-                        if s.in_waiting > 0:
-                            rx_data += s.read(s.in_waiting)
-                        if id_data in rx_data:
-                            return port_name
-                        time.sleep(0.1)
-                        i -= 1
-                    else:
-                        raise Exception("RX timeout")
-            except Exception as e:
-                if n_tries == 1:
-                    print("No oscilloscope: {}".format(e))
-            n_tries -= 1
-
+        if check_serial_port(port_name, 9600, b"*IDN?\n", b"TEKTRONIX,TDS 220"):
+            print("TDS220 found on {}".format(port_name))
+            return port_name
+        print("TDS220 not found")
+    print("No TDS220 found on any of the available ports")
     return None
 
 
@@ -42,4 +22,4 @@ if __name__ == "__main__":
 
     from src.models.list_serial_ports import get_available_serial_ports
 
-    print(get_oscilloscope_serial_port(get_available_serial_ports()))
+    print(get_tds220_serial_port(get_available_serial_ports()))
