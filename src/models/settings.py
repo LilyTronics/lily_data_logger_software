4585bc9045c015417323ba2add42f7aeeaf0a987
Danny <danny@lilytronics.nl>
2024-02-02 16:55:54 +0100

    move unit test to unit tests package

--- src/models/settings.py
+++ src/models/settings.py
@@ -6,7 +6,6 @@ import json
 import os
 
 from src.app_data import AppData
-from tests.test_suite import TestSuite
 
 
 class Settings(object):
@@ -90,46 +89,8 @@ class Settings(object):
         self._store_property("log_window", "maximized", is_maximized)
 
 
-class TestSettings(TestSuite):
-
-    def setup(self):
-        self._settings = Settings()
-        if os.path.isfile(self._settings._settings_file):
-            os.remove(self._settings._settings_file)
-
-    def test_main_window_size(self):
-        self.fail_if(self._settings.get_main_window_size() != (-1, -1), "Default settings incorrect")
-        self._settings.store_main_window_size(100, 100)
-        self.fail_if(self._settings.get_main_window_size() != (100, 100), "Failed storing settings")
-
-    def test_main_window_position(self):
-        self.fail_if(self._settings.get_main_window_position() != (-1, -1), "Default settings incorrect")
-        self._settings.store_main_window_position(10, 10)
-        self.fail_if(self._settings.get_main_window_position() != (10, 10), "Failed storing settings")
-
-    def test_main_window_maximized(self):
-        self.fail_if(self._settings.get_main_window_maximized(), "Default settings incorrect")
-        self._settings.store_main_window_maximized(True)
-        self.fail_if(not self._settings.get_main_window_maximized(), "Failed storing settings")
-        self._settings.store_main_window_maximized(False)
-
-    def test_log_window_size(self):
-        self.fail_if(self._settings.get_log_window_size() != (-1, -1), "Default settings incorrect")
-        self._settings.store_log_window_size(200, 200)
-        self.fail_if(self._settings.get_log_window_size() != (200, 200), "Failed storing settings")
-
-    def test_log_window_position(self):
-        self.fail_if(self._settings.get_log_window_position() != (-1, -1), "Default settings incorrect")
-        self._settings.store_log_window_position(20, 20)
-        self.fail_if(self._settings.get_log_window_position() != (20, 20), "Failed storing settings")
-
-    def test_log_window_maximized(self):
-        self.fail_if(self._settings.get_log_window_maximized(), "Default settings incorrect")
-        self._settings.store_log_window_maximized(True)
-        self.fail_if(not self._settings.get_log_window_maximized(), "Failed storing settings")
-        self._settings.store_log_window_maximized(False)
-
-
 if __name__ == "__main__":
+
+    from tests.unit_tests.test_settings import TestSettings
 
     TestSettings().run()
---
+++ tests/unit_tests/test_settings.py
@@ -0,0 +1,53 @@
+"""
+Test for the settings model.
+"""
+
+import os
+
+from src.models.settings import Settings
+from tests.test_suite import TestSuite
+
+
+class TestSettings(TestSuite):
+
+    def setup(self):
+        self._settings = Settings()
+        if os.path.isfile(self._settings._settings_file):
+            os.remove(self._settings._settings_file)
+
+    def test_main_window_size(self):
+        self.fail_if(self._settings.get_main_window_size() != (-1, -1), "Default settings incorrect")
+        self._settings.store_main_window_size(100, 100)
+        self.fail_if(self._settings.get_main_window_size() != (100, 100), "Failed storing settings")
+
+    def test_main_window_position(self):
+        self.fail_if(self._settings.get_main_window_position() != (-1, -1), "Default settings incorrect")
+        self._settings.store_main_window_position(10, 10)
+        self.fail_if(self._settings.get_main_window_position() != (10, 10), "Failed storing settings")
+
+    def test_main_window_maximized(self):
+        self.fail_if(self._settings.get_main_window_maximized(), "Default settings incorrect")
+        self._settings.store_main_window_maximized(True)
+        self.fail_if(not self._settings.get_main_window_maximized(), "Failed storing settings")
+        self._settings.store_main_window_maximized(False)
+
+    def test_log_window_size(self):
+        self.fail_if(self._settings.get_log_window_size() != (-1, -1), "Default settings incorrect")
+        self._settings.store_log_window_size(200, 200)
+        self.fail_if(self._settings.get_log_window_size() != (200, 200), "Failed storing settings")
+
+    def test_log_window_position(self):
+        self.fail_if(self._settings.get_log_window_position() != (-1, -1), "Default settings incorrect")
+        self._settings.store_log_window_position(20, 20)
+        self.fail_if(self._settings.get_log_window_position() != (20, 20), "Failed storing settings")
+
+    def test_log_window_maximized(self):
+        self.fail_if(self._settings.get_log_window_maximized(), "Default settings incorrect")
+        self._settings.store_log_window_maximized(True)
+        self.fail_if(not self._settings.get_log_window_maximized(), "Failed storing settings")
+        self._settings.store_log_window_maximized(False)
+
+
+if __name__ == "__main__":
+
+    TestSettings().run()
