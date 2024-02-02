60015f9400f2d6a7d8a9c5b78ae50a67c585f944
Danny <danny@lilytronics.nl>
2024-02-02 15:07:53 +0100

    refactored controller main unit test

--- src/controllers/controller_main.py
+++ src/controllers/controller_main.py
@@ -2,12 +2,10 @@
 Main controller for the application.
 """
 
-import os
-import tempfile
 import wx
 
-from src.app_data import AppData
 from src.controllers.controller_configuration import ControllerConfiguration
+from src.controllers.controller_main_test_configuration import ControllerMainTestConfiguration
 from src.models.configuration import Configuration
 from src.models.id_manager import IdManager
 from src.models.settings import Settings
@@ -129,190 +127,70 @@ class ControllerMain(object):
 
 class TestControllerMain(TestSuite):
 
-    _view_main = None
+    view_main = None
 
-    @staticmethod
-    def setup():
-        if not os.path.isdir(AppData.USER_FOLDER):
-            os.makedirs(AppData.USER_FOLDER)
-
-    def _wait_until_view_available(self):
+    def _wait_until_view_main_available(self):
         self._error = ""
         t = 2
         while t > 0:
-            if self._view_main is not None:
+            if self.view_main is not None:
                 return True
             self.sleep(0.1)
             t -= 0.1
         self._error = "View main did not load"
         return False
 
-    def _show_view_main(self, test_thread):
+    def _show_view_main(self, test_function_to_run):
+        def _test_thread(test_function):
+            if self._wait_until_view_main_available():
+                if self.gui.is_window_available(IdManager.ID_LABEL_ELAPSED_TIME):
+                    self.log.debug("Run test function: '{}()'".format(test_function.__name__))
+                    self._error = test_function(self)
+                else:
+                    self._error = "The main view did not appear"
+            else:
+                self._error = "The main view did not appear"
+
         self._error = ""
-        self.start_thread(test_thread)
+        self.start_thread(_test_thread, (test_function_to_run, ))
         app = wx.App(redirect=False)
         controller = ControllerMain("ControllerMain Test", self.log)
-        self._view_main = controller.get_view_main()
+        self.view_main = controller.get_view_main()
         app.MainLoop()
         self.fail_if(self._error != "", self._error)
-        self._view_main = None
+        self.view_main = None
 
-    def _check_configuration_values(self, sample_time, end_time, total_samples):
-        wx.Yield()
-        self.sleep(0.2)
-        value = self.gui.get_value_from_window(IdManager.ID_LABEL_SAMPLE_TIME)
-        if value != sample_time:
-            self._error = ("The sample time does not have the correct value: '{}', expected '{}'".
-                           format(value, sample_time))
-            return
-        value = self.gui.get_value_from_window(IdManager.ID_LABEL_END_TIME)
-        if value != end_time:
-            self._error = ("The end time does not have the correct value: '{}', expected '{}'".
-                           format(value, end_time))
-            return
-        # When continuous mode, total samples is not interesting
-        if total_samples is not None:
-            value = self.gui.get_value_from_window(IdManager.ID_LABEL_TOTAL_SAMPLES)
-            if value != total_samples:
-                self._error = ("The total samples does not have the correct value: '{}', expected '{}'".
-                               format(value, total_samples))
-                return
+    #######################
+    # Test show view main #
+    #######################
 
     def test_show_view_main(self):
-        def _test_show_view_main():
-            if self._wait_until_view_available():
-                if not self.gui.is_window_available(IdManager.ID_LIST_INSTRUMENTS):
-                    self._error = "The view main was not shown properly"
-                self._view_main.Close()
-
+        def _test_show_view_main(test_suite):
+            test_suite.view_main.Close()
+            return ""
         self._show_view_main(_test_show_view_main)
 
-    def test_configuration_default_values(self):
-        def _test_configuration_default_values():
-            if self._wait_until_view_available():
-                if self.gui.is_window_available(IdManager.ID_LABEL_TOTAL_SAMPLES):
-                    self.log.debug("Check default configuration")
-                    self._check_configuration_values("00:00:03", "00:01:00", "21")
-                    self._view_main.Close()
+    ######################
+    # Test configuration #
+    ######################
 
-        self._show_view_main(_test_configuration_default_values)
+    def test_configuration_default_values(self):
+        self._show_view_main(ControllerMainTestConfiguration.test_configuration_default_values)
 
     def test_cancel_edit_configuration(self):
-        def _test_cancel_edit_configuration():
-            if self._wait_until_view_available():
-                if self.gui.is_window_available(IdManager.ID_LABEL_TOTAL_SAMPLES):
-                    self.log.debug("Edit configuration")
-                    self.gui.click_toolbar_item(self._view_main, IdManager.ID_TOOL_EDIT_CONFIGURATION)
-                    if self.gui.wait_until_window_available(IdManager.ID_END_TIME):
-                        self.gui.set_value_in_control(IdManager.ID_SAMPLE_TIME, "5")
-                        self.gui.set_value_in_control(IdManager.ID_END_TIME, "3")
-                        self.gui.click_button(wx.ID_CANCEL)
-                        self._check_configuration_values("00:00:03", "00:01:00", "21")
-                    self._view_main.Close()
-
-        self._show_view_main(_test_cancel_edit_configuration)
+        self._show_view_main(ControllerMainTestConfiguration.test_cancel_edit_configuration)
 
     def test_edit_configuration_fixed_mode(self):
-        def _test_edit_configuration_fixed_mode():
-            if self._wait_until_view_available():
-                if self.gui.is_window_available(IdManager.ID_LABEL_TOTAL_SAMPLES):
-                    self.log.debug("Edit configuration")
-                    self.gui.click_toolbar_item(self._view_main, IdManager.ID_TOOL_EDIT_CONFIGURATION)
-                    if self.gui.wait_until_window_available(IdManager.ID_END_TIME):
-                        self.gui.set_value_in_control(IdManager.ID_SAMPLE_TIME, "5")
-                        self.gui.set_value_in_control(IdManager.ID_END_TIME, "3")
-                        self.gui.click_button(wx.ID_OK)
-                        self._check_configuration_values("00:00:05", "00:03:00", "37")
-                    self.gui.post_event(self._view_main, wx.wxEVT_CLOSE_WINDOW, self._view_main.GetId())
-                    self.log.debug("Check for configuration changed dialog")
-                    if self.gui.wait_for_dialog(self._view_main):
-                        self.gui.send_key_press(self.gui.KEY_TAB)
-                        self.gui.send_key_press(self.gui.KEY_ENTER)
-                        if not self.gui.wait_for_dialog(self._view_main, False):
-                            self.fail("Changed configuration dialog did not close")
-                    else:
-                        self.fail("No changed configuration dialog appeared")
-
-        self._show_view_main(_test_edit_configuration_fixed_mode)
+        self._show_view_main(ControllerMainTestConfiguration.test_edit_configuration_fixed_mode)
 
     def test_edit_configuration_continuous_mode(self):
-        def _test_edit_configuration_continuous_mode():
-            if self._wait_until_view_available():
-                if self.gui.is_window_available(IdManager.ID_LABEL_TOTAL_SAMPLES):
-                    self.log.debug("Edit configuration")
-                    self.gui.click_toolbar_item(self._view_main, IdManager.ID_TOOL_EDIT_CONFIGURATION)
-                    if self.gui.wait_until_window_available(IdManager.ID_END_TIME):
-                        self.gui.set_value_in_control(IdManager.ID_SAMPLE_TIME, "5")
-                        self.gui.set_value_in_control(IdManager.ID_END_TIME, "3")
-                        self.gui.select_radio_button(IdManager.ID_CONTINUOUS)
-                        self.gui.click_button(wx.ID_OK)
-                        self._check_configuration_values("00:00:05", "Continuous mode", None)
-                    self.gui.post_event(self._view_main, wx.wxEVT_CLOSE_WINDOW, self._view_main.GetId())
-                    self.log.debug("Check for configuration changed dialog")
-                    if self.gui.wait_for_dialog(self._view_main):
-                        self.gui.send_key_press(self.gui.KEY_TAB)
-                        self.gui.send_key_press(self.gui.KEY_ENTER)
-                        if not self.gui.wait_for_dialog(self._view_main, False):
-                            self.fail("Changed configuration dialog did not close")
-                    else:
-                        self.fail("No changed configuration dialog appeared")
-
-        self._show_view_main(_test_edit_configuration_continuous_mode)
+        self._show_view_main(ControllerMainTestConfiguration.test_edit_configuration_continuous_mode)
 
     def test_open_configuration(self):
-        def _test_open_configuration():
-            if self._wait_until_view_available():
-                if self.gui.is_window_available(IdManager.ID_LABEL_TOTAL_SAMPLES):
-                    self.log.debug("Open configuration from file: {}".format(self.configuration_test_filename))
-                    self.gui.click_toolbar_item(self._view_main, IdManager.ID_TOOL_OPEN_CONFIGURATION)
-                    self.log.debug("Check for open configuration dialog")
-                    if self.gui.wait_for_dialog(self._view_main):
-                        self.gui.send_text(self.configuration_test_filename)
-                        self.gui.send_key_press(self.gui.KEY_ENTER)
-                        self._check_configuration_values("00:00:04", "00:05:00", "76")
-                    else:
-                        self.fail("No open configuration file dialog appeared")
-                    self._view_main.Close()
-
-        self._show_view_main(_test_open_configuration)
+        self._show_view_main(ControllerMainTestConfiguration.test_open_configuration)
 
     def test_save_configuration(self):
-        def _test_save_configuration():
-            if self._wait_until_view_available():
-                if self.gui.is_window_available(IdManager.ID_LABEL_TOTAL_SAMPLES):
-                    self.log.debug("Change configuration before saving")
-                    self.gui.click_toolbar_item(self._view_main, IdManager.ID_TOOL_EDIT_CONFIGURATION)
-                    if self.gui.wait_until_window_available(IdManager.ID_END_TIME):
-                        self.gui.set_value_in_control(IdManager.ID_SAMPLE_TIME, "5")
-                        self.gui.set_value_in_control(IdManager.ID_END_TIME, "3")
-                        self.gui.click_button(wx.ID_OK)
-                        self._check_configuration_values("00:00:05", "00:03:00", "37")
-                        self.gui.click_toolbar_item(self._view_main, IdManager.ID_TOOL_SAVE_CONFIGURATION)
-                        self.log.debug("Check for save configuration dialog")
-                        if self.gui.wait_for_dialog(self._view_main):
-                            self.gui.send_text(self._filename)
-                            self.gui.send_key_press(self.gui.KEY_ENTER)
-                            # We need some time to save
-                            self.sleep(0.2)
-                        else:
-                            self.fail("No save configuration file dialog appeared")
-                        self._view_main.Close()
-
-        self._filename = tempfile.mktemp(suffix=".json")
-        self._show_view_main(_test_save_configuration)
-
-        self.log.debug("Check values in the saved configuration")
-        conf = Configuration()
-        conf.load_from_file(self._filename)
-        self.fail_if(conf.get_sample_time() != 5,
-                     "Sample time was not saved correct: {}, expected 5".format(conf.get_sample_time()))
-        self.fail_if(conf.get_end_time() != 180,
-                     "End time was not saved correct: {}, expected 180".format(conf.get_end_time()))
-        self.fail_if(conf.get_continuous_mode(),
-                     "Continuous mode was not saved correct: {}, expected False".format(conf.get_continuous_mode()))
-
-        if os.path.isfile(self._filename):
-            os.remove(self._filename)
+        self._show_view_main(ControllerMainTestConfiguration.test_save_configuration)
 
 
 if __name__ == "__main__":
---
+++ src/controllers/controller_main_test_configuration.py
@@ -0,0 +1,165 @@
+"""
+Configuration test for the main controller.
+"""
+
+import os
+import tempfile
+import wx
+
+from src.models.configuration import Configuration
+from src.models.id_manager import IdManager
+
+
+class ControllerMainTestConfiguration(object):
+
+    @classmethod
+    def _open_edit_configuration(cls, test_suite):
+        test_suite.gui.click_toolbar_item(test_suite.view_main, IdManager.ID_TOOL_EDIT_CONFIGURATION)
+        if test_suite.gui.wait_until_window_available(IdManager.ID_END_TIME):
+            return ""
+        return "Edit configuration view did not appear"
+
+    @staticmethod
+    def _check_configuration_values(test_suite, sample_time, end_time, total_samples):
+        wx.Yield()
+        test_suite.sleep(0.2)
+        value = test_suite.gui.get_value_from_window(IdManager.ID_LABEL_SAMPLE_TIME)
+        if value != sample_time:
+            return "The sample time does not have the correct value: '{}', expected '{}'".format(value, sample_time)
+        value = test_suite.gui.get_value_from_window(IdManager.ID_LABEL_END_TIME)
+        if value != end_time:
+            return "The end time does not have the correct value: '{}', expected '{}'".format(value, end_time)
+        # When continuous mode, total samples is not interesting
+        if total_samples is not None:
+            value = test_suite.gui.get_value_from_window(IdManager.ID_LABEL_TOTAL_SAMPLES)
+            if value != total_samples:
+                return "The total samples does not have the correct value: '{}', expected '{}'".format(value,
+                                                                                                       total_samples)
+        return ""
+
+    @staticmethod
+    def _close_main_view(test_suite, expect_dialog=True):
+        result = ""
+        test_suite.gui.post_event(test_suite.view_main, wx.wxEVT_CLOSE_WINDOW, test_suite.view_main.GetId())
+        test_suite.log.debug("Check for configuration changed dialog")
+        if test_suite.gui.wait_for_dialog(test_suite.view_main):
+            if not expect_dialog:
+                result += "\nConfiguration changed dialog did appear while we did not expect it"
+            test_suite.gui.send_key_press(test_suite.gui.KEY_TAB)
+            test_suite.gui.send_key_press(test_suite.gui.KEY_ENTER)
+            if not test_suite.gui.wait_for_dialog(test_suite.view_main, False):
+                result += "\nChanged configuration dialog did not close"
+        elif expect_dialog:
+            result += "\nConfiguration changed dialog did not appear"
+        return result
+
+    @classmethod
+    def test_configuration_default_values(cls, test_suite):
+        test_suite.log.debug("Check default configuration")
+        result = cls._check_configuration_values(test_suite, "00:00:03", "00:01:00", "21")
+        test_suite.view_main.Close()
+        return result
+
+    @classmethod
+    def test_cancel_edit_configuration(cls, test_suite):
+        test_suite.log.debug("Edit cancel configuration")
+        result = cls._open_edit_configuration(test_suite)
+        if result == "":
+            test_suite.gui.set_value_in_control(IdManager.ID_SAMPLE_TIME, "5")
+            test_suite.gui.set_value_in_control(IdManager.ID_END_TIME, "3")
+            test_suite.gui.click_button(wx.ID_CANCEL)
+            result = cls._check_configuration_values(test_suite, "00:00:03", "00:01:00", "21")
+            result += cls._close_main_view(test_suite, False)
+        return result
+
+    @classmethod
+    def test_edit_configuration_fixed_mode(cls, test_suite):
+        test_suite.log.debug("Edit configuration fixed mode")
+        result = cls._open_edit_configuration(test_suite)
+        if result == "":
+            test_suite.gui.set_value_in_control(IdManager.ID_SAMPLE_TIME, "5")
+            test_suite.gui.set_value_in_control(IdManager.ID_END_TIME, "3")
+            test_suite.gui.click_button(wx.ID_OK)
+            result = cls._check_configuration_values(test_suite, "00:00:05", "00:03:00", "37")
+            result += cls._close_main_view(test_suite)
+        return result.strip()
+
+    @classmethod
+    def test_edit_configuration_continuous_mode(cls, test_suite):
+        test_suite.log.debug("Edit configuration continuous mode")
+        result = cls._open_edit_configuration(test_suite)
+        if result == "":
+            test_suite.gui.set_value_in_control(IdManager.ID_SAMPLE_TIME, "5")
+            test_suite.gui.set_value_in_control(IdManager.ID_END_TIME, "3")
+            test_suite.gui.select_radio_button(IdManager.ID_CONTINUOUS)
+            test_suite.gui.click_button(wx.ID_OK)
+            result = cls._check_configuration_values(test_suite, "00:00:05", "Continuous mode", None)
+            result += cls._close_main_view(test_suite)
+        return result
+
+    @classmethod
+    def test_open_configuration(cls, test_suite):
+        test_suite.log.debug("Open configuration from file: {}".format(test_suite.configuration_test_filename))
+        test_suite.gui.click_toolbar_item(test_suite.view_main, IdManager.ID_TOOL_OPEN_CONFIGURATION)
+        test_suite.log.debug("Check for open configuration dialog")
+        if test_suite.gui.wait_for_dialog(test_suite.view_main):
+            test_suite.gui.send_text(test_suite.configuration_test_filename)
+            test_suite.gui.send_key_press(test_suite.gui.KEY_ENTER)
+            if not test_suite.gui.wait_for_dialog(test_suite.view_main, False):
+                result = "Open configuration dialog did not close"
+            else:
+                result = cls._check_configuration_values(test_suite, "00:00:04", "00:05:00", "76")
+        else:
+            result = "No open configuration file dialog appeared"
+        result += cls._close_main_view(test_suite)
+        return result.strip()
+
+    @classmethod
+    def test_save_configuration(cls, test_suite):
+        filename = tempfile.mktemp(suffix=".json")
+        test_suite.log.debug("Change configuration before saving")
+        result = cls._open_edit_configuration(test_suite)
+        if result == "":
+            test_suite.gui.set_value_in_control(IdManager.ID_SAMPLE_TIME, "5")
+            test_suite.gui.set_value_in_control(IdManager.ID_END_TIME, "3")
+            test_suite.gui.click_button(wx.ID_OK)
+            result = cls._check_configuration_values(test_suite, "00:00:05", "00:03:00", "37")
+
+        if result == "":
+            test_suite.log.debug("Save configuration to file: {}".format(filename))
+            test_suite.gui.click_toolbar_item(test_suite.view_main, IdManager.ID_TOOL_SAVE_CONFIGURATION)
+            if test_suite.gui.wait_for_dialog(test_suite.view_main):
+                test_suite.gui.send_text(filename)
+                test_suite.gui.send_key_press(test_suite.gui.KEY_ENTER)
+                if not test_suite.gui.wait_for_dialog(test_suite.view_main, False):
+                    result = "Save configuration dialog did not close"
+            else:
+                result = "No save configuration file dialog appeared"
+
+        if result == "":
+            if not os.path.isfile(filename):
+                result = "No file is created"
+            else:
+                test_suite.log.debug("Check values in the saved configuration file")
+                conf = Configuration()
+                conf.load_from_file(filename)
+                if conf.get_sample_time() != 5:
+                    result = "Sample time was not saved correct: {}, expected 5".format(conf.get_sample_time())
+                if result == "" and conf.get_end_time() != 180:
+                    result = "End time was not saved correct: {}, expected 180".format(conf.get_end_time())
+                if result == "" and conf.get_continuous_mode():
+                    result = "Continuous mode was not saved correct: {}, expected False".format(
+                        conf.get_continuous_mode())
+
+        if os.path.isfile(filename):
+            os.remove(filename)
+
+        result += cls._close_main_view(test_suite)
+        return result.strip()
+
+
+if __name__ == "__main__":
+
+    from src.controllers.controller_main import TestControllerMain
+
+    TestControllerMain().run()
