59f6fa0f658c97accf7ffa4e073d2540cee2441c
Danny <danny@lilytronics.nl>
2024-02-02 16:26:00 +0100

    move unit test to unit tests package

--- src/models/instruments/__init__.py
+++ src/models/instruments/__init__.py
@@ -5,15 +5,11 @@ Instruments package.
 import glob
 import json
 import os
-import re
-import shutil
-import tests
 
 from src.app_data import AppData
 from src.models.instrument import Instrument
 from src.models.instruments.arduino_uno_daq import arduino_uno_daq
 from src.models.instruments.simulator_multimeter import simulator_multimeter
-from tests.test_suite import TestSuite
 
 
 _INSTRUMENTS = [
@@ -56,44 +52,8 @@ def _get_instruments():
     return instruments
 
 
-class TestInstruments(TestSuite):
-
-    _instrument_def = r"\w+ = Instrument\({"
-    _n_instruments = 0
-
-    def setup(self):
-        n_found = 0
-        path = os.path.dirname(__file__)
-        self.log.debug("Looking for instruments in: {}".format(path))
-        for item in glob.glob(os.path.join(path, "*.py")):
-            content = open(item, 'r').read()
-            matches = re.findall(self._instrument_def, content)
-            if len(matches) == 1:
-                n_found += 1
-        self._n_instruments = n_found
-        self.log.debug("Copy test instrument to user folder")
-        shutil.copy2(os.path.join(os.path.dirname(tests.__file__), "test_files", "test_instrument.json"),
-                     os.path.join(AppData.USER_FOLDER, "test_instrument.json"))
-        self._n_instruments += 1
-
-    def test_instruments(self):
-        count = 0
-        for name in get_instrument_names():
-            instrument = get_instrument_by_name(name)
-            self.log.debug("{:30}: {}".format(name, instrument))
-            self.fail_if(instrument is None, "Instrument not found")
-            count += 1
-        self.fail_if(count != self._n_instruments, "The number of instruments is not correct, expecting {}".format(
-            self._n_instruments))
-        instrument = get_instrument_by_name("Unknown instrument name")
-        self.fail_if(instrument is not None, "Unknown instrument name did not return None")
-
-    @staticmethod
-    def teardown():
-        if os.path.isfile(os.path.join(AppData.USER_FOLDER, "test_instrument.json")):
-            os.remove(os.path.join(AppData.USER_FOLDER, "test_instrument.json"))
-
-
 if __name__ == "__main__":
+
+    from tests.unit_tests.test_instruments import TestInstruments
 
     TestInstruments().run()
