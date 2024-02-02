6e926dde6ce6a69cdf08a8f155ac84998bffb290
Danny <danny@lilytronics.nl>
2024-02-02 16:36:42 +0100

    move unit test to unit tests package

--- src/models/interfaces/serial_port_interface.py
+++ src/models/interfaces/serial_port_interface.py
@@ -1,15 +1,11 @@
 """
 Serial port interface.
-The unit test requires a serial port, with a loopback connector connected (automatically detected).
 """
 
 import serial
 import time
 
-from src.models.list_serial_ports import get_available_serial_ports
 from src.models.interfaces.interface import Interface
-from tests.test_environment.check_serial_loopback import get_serial_loopback_port
-from tests.test_suite import TestSuite
 
 
 class SerialPortInterface(Interface):
@@ -55,35 +51,8 @@ class SerialPortInterface(Interface):
         self._serial.close()
 
 
-class TestSerialPortInterface(TestSuite):
-
-    _CHECK_FOR_LOOPBACK_DATA = b"check_for_loopback"
-    _TEST_COMMAND = b"serial_port_test"
-    _RX_TIMEOUT = 1
-
-    _serial = None
-
-    def setup(self):
-        self.log.info("Get available serial ports")
-        ports = get_available_serial_ports()
-        self.fail_if(len(ports) == 0, "No serial ports available")
-        port = get_serial_loopback_port(ports)
-        self.log.info("Loopback found on port: {}".format(port))
-        self._serial = SerialPortInterface(port, self._RX_TIMEOUT)
-
-    def test_send_command(self):
-        response = self._serial.send_command(self._TEST_COMMAND)
-        self.fail_if(self._TEST_COMMAND + b"\n" != response, "Invalid response received: {}".format(response))
-
-    def test_no_response(self):
-        response = self._serial.send_command(self._TEST_COMMAND, False)
-        self.fail_if(response is not None, "Invalid response received: {}".format(response))
-
-    def teardown(self):
-        if self._serial is not None:
-            self._serial.close()
-
-
 if __name__ == "__main__":
+
+    from tests.unit_tests.test_serial_port_interface import TestSerialPortInterface
 
     TestSerialPortInterface().run()
