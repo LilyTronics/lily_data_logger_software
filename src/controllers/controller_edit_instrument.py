807cd3b138984385db4905c67a57818c8eab20db
Danny <danny@lilytronics.nl>
2024-02-05 07:31:50 +0100

    change method to static method

---
+++ src/controllers/controller_edit_instrument.py
@@ -0,0 +1,16 @@
+"""
+Controller for editing instruments.
+"""
+
+import wx
+
+
+class ControllerEditInstrument(object):
+
+    @staticmethod
+
+if __name__ == "__main__":
+
+    app = wx.App(redirect=False)
+
+    ControllerEditInstrument.add_instrument()
