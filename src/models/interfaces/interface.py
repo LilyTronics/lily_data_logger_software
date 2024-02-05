3ba7a55008c234648d8a907ede971f89017d32f0
Danny <danny@lilytronics.nl>
2024-02-05 09:27:53 +0100

    add method for getting settings controls

--- src/models/interfaces/interface.py
+++ src/models/interfaces/interface.py
@@ -22,3 +22,7 @@ class Interface(object):
 
     def close(self):
         raise NotImplementedError("This method must be implemented in the derived class")
+
+    @classmethod
+    def get_settings_controls(cls):
+        raise NotImplementedError("This method must be implemented in the derived class")
