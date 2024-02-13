b89ccebfbdb1317b8756ab1028873f4aeba8f089
Danny <danny@lilytronics.nl>
2024-02-13 08:21:48 +0100

    add method to check if an interface is matching a certain criteria

--- src/models/interfaces/interface.py
+++ src/models/interfaces/interface.py
@@ -5,6 +5,9 @@ Base class for all the interfaces.
 
 class Interface(object):
 
+    def __init__(self, params_to_match):
+        self._params_to_match = params_to_match
+
     def __del__(self):
         try:
             self.close()
@@ -23,6 +26,21 @@ class Interface(object):
     def close(self):
         raise NotImplementedError("This method must be implemented in the derived class")
 
+    def is_match(self, params):
+        is_match = True
+        for key in self._params_to_match.keys():
+            if key not in params.keys() or params[key] != self._params_to_match[key]:
+                is_match = False
+                break
+        return is_match
+
     @classmethod
     def get_settings_controls(cls):
         raise NotImplementedError("This method must be implemented in the derived class")
+
+
+if __name__ == "__main__":
+
+    from tests.unit_tests.test_interface import TestInterface
+
+    TestInterface().run(True)
---
+++ tests/unit_tests/test_interface.py
@@ -0,0 +1,39 @@
+"""
+Test the interface base class.
+"""
+
+from src.models.interfaces.interface import Interface
+from tests.unit_tests.lib.test_suite import TestSuite
+
+
+class TestInterface(TestSuite):
+
+    class _TestInterface(Interface):
+
+        def __init__(self, param1, param2, param3):
+            params_to_match = {
+                "param1": param1,
+                "param2": param2,
+            }
+            super().__init__(params_to_match)
+            self._params = (param1, param2, param3)
+
+    def test_is_match(self):
+        test_interface = self._TestInterface("value1", "value2", "value3")
+        self.log.debug("Match with the same parameters and values should be a match")
+        match = test_interface.is_match({"param1": "value1", "param2": "value2"})
+        self.log.debug("Is match: {}".format(match))
+        self.fail_if(not match, "The interface did not match with the same parameters")
+        self.log.debug("Match with a different value should not be a match")
+        match = test_interface.is_match({"param1": "value1", "param2": "value3"})
+        self.log.debug("Is match: {}".format(match))
+        self.fail_if(match, "The interface did match with a different value")
+        self.log.debug("Match with a missing parameter should not be a match")
+        match = test_interface.is_match({"param1": "value1", "param3": "value3"})
+        self.log.debug("Is match: {}".format(match))
+        self.fail_if(match, "The interface did match with a missing parameter")
+
+
+if __name__ == "__main__":
+
+    TestInterface().run(True)
