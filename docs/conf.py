af5120d9839fcd3b24129c91f466e051421cbc9b
Danny <danny@lilytronics.nl>
2024-02-15 13:25:15 +0100

    add sphinx stuff for creating documentation

---
+++ docs/conf.py
@@ -0,0 +1,30 @@
+"""
+Configuration file for the Sphinx documentation builder.
+
+For the full list of built-in configuration values, see the documentation:
+https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""
+
+# Disable message for naming convention in this file,
+# because we must comply to the sphinx naming convention
+# pylint: disable-msg=invalid-name
+
+import os
+import sys
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+project = 'lily-data-logger-studio-ce'
+project_copyright = '2024, LilyTronics'
+author = 'LilyTronics'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+# html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
---
+++ docs/index.rst
@@ -0,0 +1,8 @@
+Lily Data Logger Studio CE (Community Edition)
+==============================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+    Home                <self>
