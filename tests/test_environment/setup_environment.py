b0aa4f8321927f756e47400be5f01765312d244d
Danny <danny@lilytronics.nl>
2024-02-18 20:25:49 +0100

    fix some pylint issues

--- tests/test_environment/setup_environment.py
+++ tests/test_environment/setup_environment.py
@@ -11,7 +11,7 @@ from tests.test_environment.check_serial_loopback import get_serial_loopback_por
 
 
 def clear_reports(report_path):
-    print("Clear report path: {}".format(report_path))
+    print(f"Clear report path: {report_path}")
     for item in os.listdir(report_path):
         full_path = os.path.join(report_path, item)
         if os.path.isfile(full_path):
@@ -19,7 +19,7 @@ def clear_reports(report_path):
         elif os.path.isdir(full_path):
             shutil.rmtree(full_path)
         else:
-            raise Exception("ERROR: could not remove item '{}'".format(item))
+            raise Exception(f"ERROR: could not remove item '{item}'")
 
 
 def check_for_instruments(skip_instruments=False):
@@ -32,7 +32,7 @@ def check_for_instruments(skip_instruments=False):
         exclude_tests = []
         print("Detect available serial ports")
         serial_ports = get_available_serial_ports()
-        print("Available ports: {}".format(serial_ports))
+        print(f"Available ports: {serial_ports}")
         if get_serial_loopback_port(serial_ports) is None:
             exclude_tests.append("TestSerialPortInterface")
         if get_arduino_daq_serial_port(serial_ports) is None:
@@ -43,5 +43,8 @@ def check_for_instruments(skip_instruments=False):
 
 if __name__ == "__main__":
 
+    import pylint
+
     clear_reports("..\\test_reports")
     print("Exclude:", check_for_instruments())
+    pylint.run_pylint([__file__])
