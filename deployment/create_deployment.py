9ca75e7eeba0b37f7cf5a4cddc7147745deb3ffb
Danny <danny@lilytronics.nl>
2024-02-18 20:18:39 +0100

    fix some pylint issues

--- deployment/create_deployment.py
+++ deployment/create_deployment.py
@@ -3,10 +3,10 @@ Creates a deployment package.
 """
 
 import os
-import PyInstaller.__main__
 import shutil
-import src
 import zipfile
+import PyInstaller.__main__
+import src
 
 from src.app_data import AppData
 
@@ -27,10 +27,10 @@ def _clean_output_folder(output_folder):
 def _create_version_file(version_file, artifacts_path):
     print("Create version file . . .")
     version_template = os.path.join(artifacts_path, "version.template")
-    version_tuple = list(map(lambda x: int(x), AppData.VERSION.split('.')))
+    version_tuple = list(map(int, AppData.VERSION.split('.')))
     while len(version_tuple) < 4:
         version_tuple.append(0)
-    with open(version_template, "r") as fp:
+    with open(version_template, "r", encoding="utf-8") as fp:
         version_template = fp.read()
         version_template = version_template.replace("{app_name}", AppData.APP_NAME)
         version_template = version_template.replace("{version_tuple}", str(version_tuple))
@@ -38,13 +38,13 @@ def _create_version_file(version_file, artifacts_path):
         version_template = version_template.replace("{exe_name}", AppData.EXE_NAME)
         version_template = version_template.replace("{company_name}", AppData.COMPANY)
 
-    with open(version_file, "w") as fp:
+    with open(version_file, "w", encoding="utf-8") as fp:
         fp.write(version_template)
 
 
 def _create_zip_file(dist_path, artifacts_path):
     print("Create ZIP file for distribution . . .")
-    zip_filename = os.path.join(dist_path, "{}_{}.zip".format(AppData.EXE_NAME, AppData.VERSION))
+    zip_filename = os.path.join(dist_path, f"{AppData.EXE_NAME}_{AppData.VERSION}.zip")
     with zipfile.ZipFile(zip_filename, "w") as zip_object:
         # Add dist files
         app_path = os.path.join(dist_path, AppData.EXE_NAME)
@@ -53,12 +53,12 @@ def _create_zip_file(dist_path, artifacts_path):
             for filename in filenames:
                 full_path = os.path.join(current_folder, filename)
                 target_name = full_path[len(app_path) + 1:]
-                print("Add: {} to zip as: {}".format(full_path, target_name))
+                print(f"Add: {full_path} to zip as: {target_name}")
                 zip_object.write(full_path, target_name)
         # Add arduino DAQ sketch
         sketch_path = os.path.join(artifacts_path, "lily_arduino_daq", "lily_arduino_daq.ino")
         target_name = os.path.join("lily_arduino_daq", "lily_arduino_daq.ino")
-        print("Add: {} to zip as: {}".format(sketch_path, target_name))
+        print(f"Add: {sketch_path} to zip as: {target_name}")
         zip_object.write(sketch_path, target_name)
 
 
@@ -70,7 +70,7 @@ def create_deployment():
     dist_path = os.path.join(output_folder, "dist")
     version_file = os.path.join(output_folder, "app.version")
 
-    print("\n{}".format("=" * 120))
+    print(f"\n{("=" * 120)}")
     print("Build settings:")
     print("Init file       :", init_file)
     print("Application icon:", icon_file)
@@ -78,7 +78,7 @@ def create_deployment():
     print("Output folder   :", output_folder)
     print("Dist folder     :", dist_path)
     print("Version file    :", version_file)
-    print("{}\n".format("=" * 120))
+    print(f"{("=" * 120)}\n")
 
     _clean_output_folder(output_folder)
     _create_version_file(version_file, artifacts_path)
@@ -88,13 +88,13 @@ def create_deployment():
         "--clean",
         "--onedir",
         "--noconsole",
-        "--name={}".format(AppData.EXE_NAME),
-        "--icon={}".format(icon_file),
-        "--version-file={}".format(version_file),
+        f"--name={AppData.EXE_NAME}",
+        f"--icon={icon_file}",
+        f"--version-file={version_file}",
         "--contents=lib",
-        "--workpath={}".format(os.path.join(output_folder, "work")),
-        "--specpath={}".format(os.path.join(output_folder, "spec")),
-        "--distpath={}".format(dist_path)
+        f"--workpath={os.path.join(output_folder, "work")}",
+        f"--specpath={os.path.join(output_folder, "spec")}",
+        f"--distpath={dist_path}"
     ])
 
     _create_zip_file(dist_path, artifacts_path)
@@ -104,4 +104,7 @@ def create_deployment():
 
 if __name__ == "__main__":
 
+    import pylint
+
     create_deployment()
+    pylint.run_pylint([__file__])
