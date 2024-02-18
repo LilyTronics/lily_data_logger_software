cad08ff2a9cbe68db3f306de560b73121e068e66
Danny <danny@lilytronics.nl>
2024-02-18 20:20:11 +0100

    fix some pylint issues

--- tests/test_environment/check_arduino_daq.py
+++ tests/test_environment/check_arduino_daq.py
@@ -9,9 +9,10 @@ from tests.test_environment.check_serial_port import check_serial_port
 def get_arduino_daq_serial_port(serial_ports):
     print("Checking available ports for Arduino DAQ")
     for port_name in serial_ports:
-        print("Check for Arduino DAQ on port: {}".format(port_name))
-        if check_serial_port(port_name, 115200, b"rd2", [b"0\n", b"1\n"], rx_timeout=2, toggle_dtr=True):
-            print("Arduino DAQ found on {}".format(port_name))
+        print(f"Check for Arduino DAQ on port: {port_name}")
+        if check_serial_port(port_name, 115200, b"rd2", [b"0\n", b"1\n"], rx_timeout=2,
+                             toggle_dtr=True):
+            print(f"Arduino DAQ found on {port_name}")
             return port_name
         print("Arduino DAQ not found")
     print("No Arduino DAQ found on any of the available ports")
@@ -20,6 +21,8 @@ def get_arduino_daq_serial_port(serial_ports):
 
 if __name__ == "__main__":
 
+    import pylint
     from src.models.list_serial_ports import get_available_serial_ports
 
     print(get_arduino_daq_serial_port(get_available_serial_ports()))
+    pylint.run_pylint([__file__])
