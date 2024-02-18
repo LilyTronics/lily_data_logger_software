906ea40f8b9b2db8923550fb25a8ffc41e2a8c56
Danny <danny@lilytronics.nl>
2024-02-18 20:35:58 +0100

    fix some pylint issues

--- tests/unit_tests/test_configuration.py
+++ tests/unit_tests/test_configuration.py
@@ -10,8 +10,7 @@ from tests.unit_tests.lib.test_suite import TestSuite
 
 class TestConfiguration(TestSuite):
 
-    def setup(self):
-        self._filename = "test_config.json"
+    _filename = "test_config.json"
 
     def _check_default_values(self, config):
         self.fail_if(config.get_sample_time() != 3, "The default sample time is incorrect")
@@ -19,8 +18,10 @@ class TestConfiguration(TestSuite):
         self.fail_if(config.get_continuous_mode(), "The default continuous mode is incorrect")
         self.fail_if(config.get_instruments() != [], "The default instruments list is incorrect")
         self.fail_if(config.get_measurements() != [], "The default measurements list is incorrect")
-        self.fail_if(config.get_process_steps() != [], "The default process steps list is incorrect")
-        self.fail_if(config.get_filename() != "<new configuration>", "The default filename is incorrect")
+        self.fail_if(config.get_process_steps() != [],
+                     "The default process steps list is incorrect")
+        self.fail_if(config.get_filename() != "<new configuration>",
+                     "The default filename is incorrect")
         self.fail_if(config.is_changed(), "The changed flag is incorrect")
 
     def test_empty_configuration(self):
@@ -36,14 +37,12 @@ class TestConfiguration(TestSuite):
         self.fail_if(conf.get_sample_time() != new_value, "Failed to store the sample time")
         self.fail_if(not conf.is_changed(), "The changed flag is incorrect")
 
-        conf._is_changed = False
         current_value = conf.get_end_time()
         new_value = current_value + 2
         conf.set_end_time(new_value)
         self.fail_if(conf.get_end_time() != new_value, "Failed to store the end time")
         self.fail_if(not conf.is_changed(), "The changed flag is incorrect")
 
-        conf._is_changed = False
         current_value = conf.get_continuous_mode()
         new_value = not current_value
         conf.set_continuous_mode(new_value)
@@ -67,26 +66,29 @@ class TestConfiguration(TestSuite):
         self.fail_if(conf.is_changed(), "The changed flag is incorrect")
 
     def test_load_empty_file(self):
-        open(self._filename, "w").close()
+        with open(self._filename, "w", encoding="utf-8") as fp:
+            fp.close()
         conf = Configuration()
         try:
             conf.load_from_file(self._filename)
             self.fail("Reading empty file passed, expected an exception")
         except Exception as e:
-            self.log.debug("Error message:\n{}".format(e))
-            self.fail_if("Error reading file: {}".format(self._filename) not in str(e), "Error message is incorrect")
+            self.log.debug(f"Error message:\n{e}")
+            self.fail_if(f"Error reading file: {self._filename}" not in str(e),
+                         "Error message is incorrect")
         self._check_default_values(conf)
 
     def test_load_invalid_file(self):
         conf = Configuration()
-        with open(self._filename, "w") as fp:
+        with open(self._filename, "w", encoding="utf-8") as fp:
             fp.write("invalid file format\n")
         try:
             conf.load_from_file(self._filename)
             self.fail("Reading empty file passed, expected an exception")
         except Exception as e:
-            self.log.debug("Error message:\n{}".format(e))
-            self.fail_if("Error reading file: {}".format(self._filename) not in str(e), "Error message is incorrect")
+            self.log.debug(f"Error message:\n{e}")
+            self.fail_if(f"Error reading file: {self._filename}" not in str(e),
+                         "Error message is incorrect")
         self._check_default_values(conf)
 
     def test_add_update_instrument(self):
@@ -107,7 +109,7 @@ class TestConfiguration(TestSuite):
         self.fail_if(instrument is None, "Instrument was not found, using the name")
         self.fail_if(conf.KEY_ID not in instrument.keys(), "Instrument has no ID")
         instrument_id = instrument[conf.KEY_ID]
-        self.log.debug("Instrument has ID: {}".format(instrument_id))
+        self.log.debug(f"Instrument has ID: {instrument_id}")
         instrument = conf.get_instrument(instrument_id)
         self.fail_if(instrument is None, "Instrument was not found, using the ID")
         self.log.debug("Update instrument")
@@ -120,7 +122,8 @@ class TestConfiguration(TestSuite):
         instrument = conf.get_instrument(new_name)
         self.fail_if(instrument is None, "Instrument with the new name was not found")
         self.fail_if(instrument[conf.KEY_ID] != instrument_id, "Instrument ID is not the same")
-        self.fail_if(instrument[conf.KEY_SETTINGS][conf.KEY_INSTRUMENT_SETTINGS]["ip_port"] != 18000,
+        self.fail_if(instrument[conf.KEY_SETTINGS][
+                         conf.KEY_INSTRUMENT_SETTINGS]["ip_port"] != 18000,
                      "The changed setting was not stored")
 
     def test_delete_instrument(self):
@@ -188,4 +191,7 @@ class TestConfiguration(TestSuite):
 
 if __name__ == "__main__":
 
+    import pylint
+
     TestConfiguration().run(True)
+    pylint.run_pylint([__file__])
