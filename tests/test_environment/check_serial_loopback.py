de6bbd11231ca541ab12347afc86af1080605f25
Danny <danny@lilytronics.nl>
2024-02-18 20:21:07 +0100

    fix some pylint issues

--- tests/test_environment/check_serial_loopback.py
+++ tests/test_environment/check_serial_loopback.py
@@ -8,9 +8,9 @@ from tests.test_environment.check_serial_port import check_serial_port
 def get_serial_loopback_port(serial_ports):
     print("Checking available ports for a loopback")
     for port_name in serial_ports:
-        print("Check for loopback on port: {}".format(port_name))
+        print(f"Check for loopback on port: {port_name}")
         if check_serial_port(port_name, 115200, b"CHECK_FOR_LOOPBACK", b"CHECK_FOR_LOOPBACK"):
-            print("Loopback found on {}".format(port_name))
+            print(f"Loopback found on {port_name}")
             return port_name
         print("Loopback not found")
     print("No loopback found on any of the available ports")
@@ -19,6 +19,8 @@ def get_serial_loopback_port(serial_ports):
 
 if __name__ == "__main__":
 
+    import pylint
     from src.models.list_serial_ports import get_available_serial_ports
 
     print(get_serial_loopback_port(get_available_serial_ports()))
+    pylint.run_pylint([__file__])
