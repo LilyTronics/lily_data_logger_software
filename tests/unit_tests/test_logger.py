bc3cbd4e177be024d6ea03a6610db90b984ae2b3
Danny <danny@lilytronics.nl>
2024-02-19 07:31:57 +0100

    fix some pylint issues

--- tests/unit_tests/test_logger.py
+++ tests/unit_tests/test_logger.py
@@ -15,13 +15,14 @@ class TestLogger(TestSuite):
         ("stdout", "This is a standard output message"),
         ("info", "This is a\nmulti line message"),
     ]
+    _logger = None
 
     def setup(self):
         self._logger = Logger()
 
     def test_log_file_empty(self):
         filename = self._logger.get_filename()
-        with open(filename, "r") as fp:
+        with open(filename, "r", encoding="utf-8") as fp:
             content = fp.read().strip()
         self.fail_if(content != "", "The log file is not empty")
 
@@ -37,7 +38,7 @@ class TestLogger(TestSuite):
                 print(message[1])
 
         filename = self._logger.get_filename()
-        with open(filename, "r") as fp:
+        with open(filename, "r", encoding="utf-8") as fp:
             lines = list(map(lambda x: x.rstrip(), fp.readlines()))
         self.fail_if(len(lines) == 0, "The log file is empty")
 
@@ -47,39 +48,40 @@ class TestLogger(TestSuite):
             n_found = 0
             for part in parts:
                 for line in lines:
-                    if line.endswith("| {}".format(part)) and "| {:6} |".format(message[0].upper()) in line:
+                    if line.endswith(f"| {part}") and f"| {message[0].upper():6} |" in line:
                         n_found += 1
             if n_found != len(parts):
-                self.fail("Message '{}' not found in the log file".format(message))
+                self.fail(f"Message '{message}' not found in the log file")
 
     def test_exception(self):
         def _generate_error():
-            _dummy = 1 / 0
+            _ = 1 / 0
 
         # Clear the file
         filename = self._logger.get_filename()
-        open(filename, "w").close()
+        with open(filename, "w", encoding="utf-8") as fp:
+            fp.close()
         # To prevent this from failing, we generate an error in a thread
         t = self.start_thread(_generate_error)
         while t.is_alive():
             pass
 
-        with open(filename, "r") as fp:
+        with open(filename, "r", encoding="utf-8") as fp:
             lines = list(map(lambda x: x.rstrip(), fp.readlines()))
         self.fail_if(len(lines) == 0, "The log file is empty")
         self.fail_if(len(lines) < 4, "The log file has less than 4 lines")
 
         for line in lines:
-            self.fail_if("| STDERR |" not in line, "Message is not type STDERR '{}'".format(line))
+            self.fail_if("| STDERR |" not in line, f"Message is not type STDERR '{line}'")
 
         self.fail_if("| STDERR | Exception in thread Thread" not in lines[0],
-                     "First line does not match with the exception message '{}'".format(lines[0]))
+                     f"First line does not match with the exception message '{lines[0]}'")
 
         self.fail_if("| STDERR | Traceback (most recent call last):" not in lines[1],
-                     "Second line does not match with the traceback message '{}'".format(lines[1]))
+                     f"Second line does not match with the traceback message '{lines[1]}'")
 
         self.fail_if("| STDERR | ZeroDivisionError: division by zero" not in lines[-1],
-                     "Last line does not match with the division by zero message '{}'".format(lines[-1]))
+                     f"Last line does not match with the division by zero message '{lines[-1]}'")
 
     def teardown(self):
         self._logger.shut_down()
@@ -87,4 +89,7 @@ class TestLogger(TestSuite):
 
 if __name__ == "__main__":
 
-    TestLogger().run()
+    import pylint
+
+    TestLogger().run(True)
+    pylint.run_pylint([__file__])
