dde56eb28cc1424619e1208234cd4fbead7f1ca1
Danny <danny@lilytronics.nl>
2024-02-19 07:41:22 +0100

    fix some pylint issues

--- tests/unit_tests/test_settings.py
+++ tests/unit_tests/test_settings.py
@@ -10,20 +10,25 @@ from tests.unit_tests.lib.test_suite import TestSuite
 
 class TestSettings(TestSuite):
 
+    _settings = None
+
     def setup(self):
         self._settings = Settings()
-        if os.path.isfile(self._settings._settings_file):
-            os.remove(self._settings._settings_file)
+        if os.path.isfile(self._settings.get_filename()):
+            os.remove(self._settings.get_filename())
 
     def test_main_window_size(self):
-        self.fail_if(self._settings.get_main_window_size() != (-1, -1), "Default settings incorrect")
+        self.fail_if(self._settings.get_main_window_size() != (-1, -1),
+                     "Default settings incorrect")
         self._settings.store_main_window_size(100, 100)
         self.fail_if(self._settings.get_main_window_size() != (100, 100), "Failed storing settings")
 
     def test_main_window_position(self):
-        self.fail_if(self._settings.get_main_window_position() != (-1, -1), "Default settings incorrect")
+        self.fail_if(self._settings.get_main_window_position() != (-1, -1),
+                     "Default settings incorrect")
         self._settings.store_main_window_position(10, 10)
-        self.fail_if(self._settings.get_main_window_position() != (10, 10), "Failed storing settings")
+        self.fail_if(self._settings.get_main_window_position() != (10, 10),
+                     "Failed storing settings")
 
     def test_main_window_maximized(self):
         self.fail_if(self._settings.get_main_window_maximized(), "Default settings incorrect")
@@ -37,9 +42,11 @@ class TestSettings(TestSuite):
         self.fail_if(self._settings.get_log_window_size() != (200, 200), "Failed storing settings")
 
     def test_log_window_position(self):
-        self.fail_if(self._settings.get_log_window_position() != (-1, -1), "Default settings incorrect")
+        self.fail_if(self._settings.get_log_window_position() != (-1, -1),
+                     "Default settings incorrect")
         self._settings.store_log_window_position(20, 20)
-        self.fail_if(self._settings.get_log_window_position() != (20, 20), "Failed storing settings")
+        self.fail_if(self._settings.get_log_window_position() != (20, 20),
+                     "Failed storing settings")
 
     def test_log_window_maximized(self):
         self.fail_if(self._settings.get_log_window_maximized(), "Default settings incorrect")
@@ -50,4 +57,7 @@ class TestSettings(TestSuite):
 
 if __name__ == "__main__":
 
-    TestSettings().run()
+    import pylint
+
+    TestSettings().run(True)
+    pylint.run_pylint([__file__])
