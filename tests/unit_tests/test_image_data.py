79ee3206c6ba6efc8b7fffea6edb4ca9d804a5e8
Danny <danny@lilytronics.nl>
2024-02-19 07:18:16 +0100

    fix some pylint issues

--- tests/unit_tests/test_image_data.py
+++ tests/unit_tests/test_image_data.py
@@ -20,16 +20,19 @@ class TestImageData(TestSuite):
         for item in glob.glob(os.path.join(images_path, "*.png")):
             image_name = os.path.splitext(os.path.split(item)[-1])[0]
             self.fail_if(not hasattr(ImageData, image_name),
-                         "There is no definition for image: '{}'".format(image_name))
+                         f"There is no definition for image: '{image_name}'")
             embedded_image = getattr(ImageData, image_name)
             self.fail_if(not isinstance(embedded_image, wx.lib.embeddedimage.PyEmbeddedImage),
-                         "The object for attribute '{}' is not a embedded image object".format(image_name))
+                         f"The object for attribute '{image_name}' is not a embedded image object")
             bitmap = embedded_image.Bitmap
             self.fail_if(not isinstance(bitmap, wx.Bitmap),
-                         "The bitmap for attribute '{}' is not a wx.Bitmap".format(image_name))
+                         f"The bitmap for attribute '{image_name}' is not a wx.Bitmap")
         app.Destroy()
 
 
 if __name__ == "__main__":
 
+    import pylint
+
     TestImageData().run(True)
+    pylint.run_pylint([__file__])
