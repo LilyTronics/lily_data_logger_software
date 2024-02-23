1beaa44372aa5569fe7d931f9b8cb219e7696f89
Danny <danny@lilytronics.nl>
2024-02-23 10:02:16 +0100

    send uniform messages

--- src/models/measurement_runner.py
+++ src/models/measurement_runner.py
@@ -10,21 +10,31 @@ from src.models.instrument_pool import InstrumentPool
 
 class MeasurementRunner:
 
+    _MESSAGE_TYPE_STATUS = 'status'
+    _MESSAGE_TYPE_VALUE = 'value'
+
     def __init__(self, configuration, callback):
         self._configuration = configuration
         self._callback = callback
         self._measurement_thread = None
         self._stop_event = threading.Event()
 
+    def _send_callback(self, timestamp, message_type, identifier, value):
+        # Wrapper to force uniform callback data
+        self._callback(timestamp, message_type, identifier, value)
+
     def _create_instruments(self):
-        self._callback(int(time.time()), "Create instruments")
-        for measurement in self._configuration.get_measurements():
+        self._send_callback(int(time.time()), self._MESSAGE_TYPE_STATUS, "Create instruments", 0)
+        for i, measurement in enumerate(self._configuration.get_measurements()):
             settings = measurement[self._configuration.KEY_SETTINGS]
             instrument_data = self._configuration.get_instrument(
                 settings[self._configuration.KEY_INSTRUMENT_ID])
-            self._callback(
+            self._send_callback(
                 int(time.time()),
-                f"Initialize instrument '{instrument_data[self._configuration.KEY_NAME]}'")
+                self._MESSAGE_TYPE_STATUS,
+                f"Initialize instrument '{instrument_data[self._configuration.KEY_NAME]}'",
+                i
+            )
             instrument = InstrumentPool.create_instrument(instrument_data)
             instrument.initialize()
             instrument.start()
@@ -39,9 +49,10 @@ class MeasurementRunner:
         # If the response is too late, we send an error value for the original request time
         # and the measurement value to the next valid request time
         while request_time + sample_time < response_time:
-            self._callback(request_time, measurement_name, "timing error")
+            self._send_callback(request_time, self._MESSAGE_TYPE_VALUE, measurement_name,
+                                "time error")
             request_time += sample_time
-        self._callback(request_time, measurement_name, response)
+        self._send_callback(request_time, self._MESSAGE_TYPE_VALUE, measurement_name, response)
 
     def _requests_measurements(self, timestamp):
         measurements = self._configuration.get_measurements()
@@ -61,7 +72,7 @@ class MeasurementRunner:
         InstrumentPool.clear_instruments()
         self._create_instruments()
         start_time = int(time.time())
-        self._callback(start_time, "Start measurements")
+        self._send_callback(start_time, self._MESSAGE_TYPE_STATUS, "Start measurements", 0)
         sample_start = start_time
         while not self._stop_event.is_set():
             self._requests_measurements(sample_start)
@@ -72,7 +83,7 @@ class MeasurementRunner:
                     self._stop_event.set()
                 time.sleep(0.01)
             sample_start = int(time.time())
-        self._callback(int(time.time()), "Process finished")
+        self._send_callback(int(time.time()), self._MESSAGE_TYPE_STATUS, "Process finished", 0)
 
     def start(self):
         if not self.is_running():
