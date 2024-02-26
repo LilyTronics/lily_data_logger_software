c8501b0a165589cd4dc5a889fcc630115484ca10
Danny <danny@lilytronics.nl>
2024-02-26 16:24:17 +0100

    change method for creating timestamp to accept a custom timestamp value

--- tests/unit_tests/test_models/test_time_converter.py
+++ tests/unit_tests/test_models/test_time_converter.py
@@ -43,6 +43,11 @@ class TestTimeConverter(TestSuite):
             self.fail_if(TimeConverter.convert_time_with_unit_to_seconds(*test_value[0]) !=
                          test_value[1], "Time value is incorrect")
 
+    def test_create_time_stamp(self):
+        test_value = (1708959067, "20240226 15:51:07")
+        self.fail_if(TimeConverter.get_timestamp(test_value[0]) != test_value[1],
+                     "Timestamp is not correct")
+
 
 if __name__ == "__main__":
 
