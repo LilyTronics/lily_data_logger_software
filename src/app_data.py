79aab3a006a96dd95d1b45ce00f4a6da1ae7606e
Danny <danny@lilytronics.nl>
2024-03-04 08:18:15 +0100

    set version to 1.0

--- src/app_data.py
+++ src/app_data.py
@@ -8,7 +8,7 @@ import sys
 
 class AppData:
     APP_NAME = "Lily Data Logger Studio CE"
-    VERSION = "0.9"
+    VERSION = "1.0"
     EXE_NAME = "LilyDataLoggerStudioCE"
     COMPANY = "LilyTronics"
     USER_FOLDER = os.path.join(os.path.join(os.path.expanduser("~")), EXE_NAME)
