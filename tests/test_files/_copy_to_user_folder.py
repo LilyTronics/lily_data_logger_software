dffd11648d999146cc6e0759636ebff1c2464cda
Danny <danny@lilytronics.nl>
2024-03-05 14:16:18 +0100

    delete old files before copying new ones

--- tests/test_files/_copy_to_user_folder.py
+++ tests/test_files/_copy_to_user_folder.py
@@ -9,22 +9,35 @@ import shutil
 from src.app_data import AppData
 
 
-def copy_to_user_folder():
-    n_copied = 0
-    for source in glob.glob("instrument_*.json"):
-        destination = os.path.join(AppData.USER_FOLDER, source)
-        print(f"Copy: {source} to: {destination}")
-        shutil.copy2(source, destination)
-        n_copied += 1
-    print(f"\n{n_copied} instruments copied")
+_IGNORE_FILES = [
+    os.path.join(AppData.USER_FOLDER, "LilyDataLoggerStudioCE.json"),
+    os.path.join(AppData.USER_FOLDER, "LilyDataLoggerStudioCE.log")
+]
 
+
+def _clean_user_folder():
+    matches = list(filter(lambda x: x not in _IGNORE_FILES,
+                          glob.glob(os.path.join(AppData.USER_FOLDER, "*.*"))))
+    for item in matches:
+        print(f"Delete {item}")
+        os.unlink(item)
+
+
+def _copy_items(file_filter):
     n_copied = 0
-    for source in glob.glob("configuration_*.json"):
+    for source in glob.glob(file_filter):
         destination = os.path.join(AppData.USER_FOLDER, source)
         print(f"Copy: {source} to: {destination}")
-        shutil.copy2(source, destination)
+        shutil.copy(str(source), str(destination))
         n_copied += 1
-    print(f"\n{n_copied} configurations copied")
+    return n_copied
+
+
+def copy_to_user_folder():
+    _clean_user_folder()
+    n_copied = _copy_items("instrument_*.json")
+    n_copied += _copy_items("configuration_*.json")
+    print(f"\n{n_copied} files copied")
 
 
 if __name__ == "__main__":
