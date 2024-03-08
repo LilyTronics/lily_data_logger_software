82f0a21f878a9e1e82a73564ee73a797c2aa3922
Danny <danny@lilytronics.nl>
2024-03-08 07:44:42 +0100

    check instruments using the instrument pool

--- src/controllers/controller_check_instruments.py
+++ src/controllers/controller_check_instruments.py
@@ -6,8 +6,7 @@ import threading
 import wx
 
 from src.models.id_manager import IdManager
-from src.models.instruments import Instruments
-from src.models.interface_pool import InterfacePool
+from src.models.instrument_pool import InstrumentPool
 from src.views.view_check_instruments import ViewCheckInstruments
 
 
@@ -40,7 +39,7 @@ class ControllerCheckInstruments:
                 instrument_name = instruments[i][self._config.KEY_NAME]
                 message = f"Checking instrument: '{instrument_name}' . . ."
                 self._view.active_dialog.Update(i, message)
-                t = threading.Thread(target=self._check_instrument, args=(instrument_name, ))
+                t = threading.Thread(target=self._check_instrument, args=(instruments[i], ))
                 t.daemon = True
                 t.start()
             elif not t.is_alive():
@@ -54,56 +53,26 @@ class ControllerCheckInstruments:
         self._view.active_dialog = None
         wx.YieldIfNeeded()
 
-    def _get_instrument_object(self, instrument_name):
-        instrument_data = self._config.get_instrument(instrument_name)
-        assert instrument_data is not None, (
-            f"Instrument '{instrument_name}' not in the configuration")
-        settings = instrument_data.get(self._config.KEY_SETTINGS, None)
-        assert settings is not None, f"Instrument '{instrument_name}' has no settings"
-        instrument_definition = settings.get(self._config.KEY_INSTRUMENT_NAME, None)
-        assert instrument_definition is not None, (
-            f"Instrument '{instrument_name}' has no instrument definition defined")
-        instrument_object = Instruments.get_instrument_by_name(instrument_definition)
-        assert instrument_object is not None, (
-            f"Instrument definition '{instrument_definition}' does not exist")
-        return instrument_object
-
-    def _get_instrument_settings(self, instrument_name, instrument_object):
-        instrument_data = self._config.get_instrument(instrument_name)
-        settings = instrument_data[self._config.KEY_SETTINGS]
-        instrument_defaults = instrument_object.get_interface_settings()
-        instrument_settings = settings.get(self._config.KEY_INSTRUMENT_SETTINGS, {})
-        for key in instrument_defaults.keys():
-            if key not in instrument_settings.keys():
-                instrument_settings[key] = instrument_defaults[key]
-        return instrument_settings
-
-    def _check_instrument(self, instrument_name):
+    def _check_instrument(self, instrument_data):
+        instrument_name = instrument_data[self._config.KEY_NAME]
         self._update_status(instrument_name, None, "")
-        interface_object = None
+        InstrumentPool.clear_instruments()
+        result = "Connection OK"
         try:
-            instrument_object = self._get_instrument_object(instrument_name)
-            instrument_settings = self._get_instrument_settings(instrument_name, instrument_object)
-
-            interface_object = InterfacePool.create_interface(
-                instrument_object.get_interface_type(), instrument_settings
-            )
-            instrument_object.set_interface_object(interface_object)
-            instrument_object.initialize()
-            result = "Connection OK"
-            input_channels = instrument_object.get_input_channels()
+            instrument = InstrumentPool.create_instrument(instrument_data)
+            instrument.initialize()
+            instrument.start()
+            input_channels = instrument.get_input_channels()
             if len(input_channels) > 0:
-                channel_name = input_channels[0][instrument_object.KEY_NAME]
-                value = instrument_object.process_channel(channel_name)
+                channel_name = input_channels[0][instrument.KEY_NAME]
+                value = instrument.process_channel(channel_name)
                 if isinstance(value, str) and value.startswith("ERROR: "):
                     raise Exception(value)
                 result += f" ({channel_name} = {value})"
             self._update_status(instrument_name, True, result)
         except Exception as e:
             self._update_status(instrument_name, False, str(e))
-        finally:
-            if interface_object is not None:
-                interface_object.close()
+        InstrumentPool.clear_instruments()
 
     def _update_status(self, instrument_id, is_passed, message):
         wx.CallAfter(self._view.update_instrument, instrument_id, is_passed, message)
