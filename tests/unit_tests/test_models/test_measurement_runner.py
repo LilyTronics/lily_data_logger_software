0f0417082d467bc02d6e93cf1ff5eb2fdb3aa8d4
Danny <danny@lilytronics.nl>
2024-03-19 07:57:47 +0100

    fixed time issue

--- tests/unit_tests/test_models/test_measurement_runner.py
+++ tests/unit_tests/test_models/test_measurement_runner.py
@@ -12,10 +12,15 @@ class TestMeasurementRunner(TestSuite):
 
     _runner = None
     _messages = []
+    _duration = 0
 
     def _callback(self, *args):
         self._messages.append(args)
         self.log.debug(f"Callback message: ({args})")
+        if "start" in args:
+            self._duration = args[0]
+        if "finished" in args:
+            self._duration = args[0] - self._duration
 
     def setup(self):
         Simulators.start_simulators(self.log)
@@ -24,21 +29,25 @@ class TestMeasurementRunner(TestSuite):
 
     def test_start_stop(self):
         del self._messages[:]
+        self._duration = 0
         self.log.debug("Start measurement runner")
         self._runner.start()
         self.fail_if(not self._runner.is_running(), "Measurement runner is not running")
         self.log.debug("Wait a few seconds")
-        self.sleep(3.1)
+        self.sleep(3)
         self.log.debug("Stop measurement runner")
         self._runner.stop()
         self.fail_if(self._runner.is_running(), "Measurement runner is not stopped")
-        self.fail_if(len(self._messages) < 3, "Not enough messages received")
+        self.log.debug(f"Number of messages: {len(self._messages)}")
+        self.fail_if(len(self._messages) < 11, "Not enough messages received")
+        self.log.debug(f"Duration: {self._duration}")
         self.log.debug(f"Elapsed time: {self._runner.get_elapsed_time():.1f} seconds")
-        self.fail_if(self._runner.get_elapsed_time() < 3, "Elapsed time is too small")
-        self.fail_if(self._runner.get_elapsed_time() > 4, "Elapsed time is too big")
+        self.fail_if(self._runner.get_elapsed_time() < self._duration, "Elapsed time is too small")
+        self.fail_if(self._runner.get_elapsed_time() > self._duration + 1, "Elapsed time is too big")
 
     def test_start_until_finished(self):
         del self._messages[:]
+        self._duration = 0
         self.log.debug("Start measurement runner")
         self._runner.start()
         self.log.debug("Wait for measurement runner to finish")
@@ -46,9 +55,12 @@ class TestMeasurementRunner(TestSuite):
         if not self.wait_for(self._runner.is_running, False, 10, 0.1):
             self.fail("Measurement runner did not finish by itself")
         self.fail_if(self._runner.is_running(), "Measurement runner is not stopped")
-        self.fail_if(len(self._messages) < 6, "Not enough messages received")
+        self.log.debug(f"Number of messages: {len(self._messages)}")
+        self.fail_if(len(self._messages) < 17, "Not enough messages received")
+        self.log.debug(f"Duration: {self._duration}")
         self.log.debug(f"Elapsed time: {self._runner.get_elapsed_time():.1f} seconds")
-        self.fail_if(self._runner.get_elapsed_time() < 7, "Elapsed time is too small")
+        self.fail_if(self._duration != 7, "Invalid duration from timestamps")
+        self.fail_if(self._runner.get_elapsed_time() < 6.8, "Elapsed time is too small")
         self.fail_if(self._runner.get_elapsed_time() > 7.2, "Elapsed time is too small")
 
     def teardown(self):
