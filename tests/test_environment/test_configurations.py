6ec78f118015c137d0fb4444c19e5f4e6f2a69c0
Danny <danny@lilytronics.nl>
2024-03-21 08:30:52 +0100

    create function for returning a test configuration for unit testing

--- tests/test_environment/test_configurations.py
+++ tests/test_environment/test_configurations.py
@@ -2,54 +2,84 @@
 Test configurations containing various test configurations.
 """
 
-import os
-import shutil
-
-from src.app_data import AppData
 from src.models.configuration import Configuration
+from src.simulators.simulator_settings import SimulatorSettings
 
 
 class TestConfigurations:
 
     _CONFIGURATIONS = {}
 
-    _REQUIRED_USER_FILES = [
-        "instrument_delayed_measurement.json"
-    ]
-
     ##########
     # Public #
     ##########
 
     @classmethod
-    def init(cls):
-        cls._CONFIGURATIONS["all simulators, 2s/7s"] = "config_all_simulators_2_7.json"
-        cls._CONFIGURATIONS["failing measurement"] = "config_failing_measurement.json"
-        cls._CONFIGURATIONS["delayed measurement"] = "configuration_delayed_measurement.json"
-
-    @classmethod
-    def get_configuration_names(cls):
-        return list(cls._CONFIGURATIONS)
-
-    @classmethod
-    def get_configuration(cls, name):
-        config = Configuration()
-        filename = cls._CONFIGURATIONS.get(name)
-        if filename is not None:
-            full_path = os.path.join(os.path.dirname(__file__), "test_files", filename)
-            config.load_from_file(full_path)
-        return config
-
-    @classmethod
     def get_unit_test_configuration(cls):
-        return cls.get_configuration("all simulators, 2s/7s")
+        return cls._create_all_simulators(2, 7)
 
+    ###########
+    # Private #
+    ###########
 
-# Initialize the test configurations
-TestConfigurations.init()
+    @classmethod
+    def _create_all_simulators(cls, sample_time, duration):
+        conf = Configuration()
+        conf.set_sample_time(sample_time)
+        conf.set_end_time(duration)
+        cls._add_multimeter(conf)
+        cls._add_temperature_meter(conf)
+        return conf
+
+    @staticmethod
+    def _add_multimeter(conf):
+        name = "Multimeter"
+        settings = {
+            conf.KEY_INSTRUMENT_NAME: "Simulator multimeter",
+            conf.KEY_INSTRUMENT_SETTINGS: {
+                "ip_address": SimulatorSettings.MultimeterUdp.IP,
+                "ip_port": SimulatorSettings.MultimeterUdp.PORT,
+                "rx_timeout": SimulatorSettings.MultimeterUdp.RX_TIME_OUT
+            }
+        }
+        conf.update_instrument(name, name, settings)
+        if len(conf.get_instruments()) > 0:
+            for measurement in (("Volt", "Get DC voltage"), ("Curr", "Get DC current")):
+                instrument = conf.get_instrument("Multimeter")
+                settings = {
+                    conf.KEY_INSTRUMENT_ID: instrument[conf.KEY_ID],
+                    conf.KEY_MEASUREMENT: measurement[1],
+                    conf.KEY_GAIN: 1.0,
+                    conf.KEY_OFFSET: 0.0
+                }
+                conf.update_measurement(measurement[0], measurement[0], settings)
+        return conf
+
+    @staticmethod
+    def _add_temperature_meter(conf):
+        name = "Temperature meter"
+        settings = {
+            conf.KEY_INSTRUMENT_NAME: "Simulator temperature meter",
+            conf.KEY_INSTRUMENT_SETTINGS: {
+                "ip_address": SimulatorSettings.TemperatureMeterTcp.IP,
+                "ip_port": SimulatorSettings.TemperatureMeterTcp.PORT,
+                "rx_timeout": SimulatorSettings.TemperatureMeterTcp.RX_TIME_OUT
+            }
+        }
+        conf.update_instrument(name, name, settings)
+        if len(conf.get_instruments()) > 0:
+            instrument = conf.get_instrument("Temperature meter")
+            settings = {
+                conf.KEY_INSTRUMENT_ID: instrument[conf.KEY_ID],
+                conf.KEY_MEASUREMENT: "Get temperature",
+                conf.KEY_GAIN: 1.0,
+                conf.KEY_OFFSET: 0.0
+            }
+            conf.update_measurement("Outside temperature", "Outside temperature", settings)
+        return conf
 
 
 if __name__ == "__main__":
 
-    for _name in TestConfigurations.get_configuration_names():
-        print(_name, TestConfigurations.get_configuration(_name).__class__)
+    config = TestConfigurations.get_unit_test_configuration()
+    print(config)
